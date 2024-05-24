f1332e87e8c16a86187377fcd6dfc78c65fdda19
notetoday <115170187+notetoday@users.noreply.github.com>
2024-05-24 11:28:52 +0800

    更新 MTP.sh

--- MTP.sh
+++ MTP.sh
@@ -5,12 +5,22 @@ read -p "请输入 MTP_PORT: " MTP_PORT
 read -p "请输入 MTP_SECRET: " MTP_SECRET
 read -p "请输入 MTP_TAG: " MTP_TAG
 
-# 安装 Docker
-curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh
+# 检查并安装 Docker
+if ! command -v docker &> /dev/null; then
+    echo "Docker 未安装，正在安装..."
+    curl -fsSL https://get.docker.com -o get-docker.sh && sh get-docker.sh
+else
+    echo "Docker 已安装，跳过安装步骤。"
+fi
 
-# 安装 Docker Compose
-curl -L "https://github.com/docker/compose/releases/download/v2.16.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose 
-chmod +x /usr/local/bin/docker-compose
+# 检查并安装 Docker Compose
+if ! command -v docker-compose &> /dev/null; then
+    echo "Docker Compose 未安装，正在安装..."
+    curl -L "https://github.com/docker/compose/releases/download/v2.16.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose 
+    chmod +x /usr/local/bin/docker-compose
+else
+    echo "Docker Compose 已安装，跳过安装步骤。"
+fi
 
 # 创建部署目录和 docker-compose.yml 文件
 mkdir -p ~/deploy/mtproto
