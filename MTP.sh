0fdfd3919545d2a6a38e9f6e2366e2169026b85b
notetoday <115170187+notetoday@users.noreply.github.com>
2024-05-24 12:42:55 +0800

    更新 MTP.sh

--- MTP.sh
+++ MTP.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 
-# 生成随机的 32 位十六进制字符串作为 MTP_SECRET
-generate_random_secret() {
-    tr -dc 'a-f0-9' < /dev/urandom | head -c 32
-}
-
-# 提示用户输入 MTP_PORT 和 MTP_TAG
+# 提示用户输入 MTP_PORT
 while true; do
     read -p "请输入 MTP_PORT (1-65535): " MTP_PORT
     if [[ $MTP_PORT -ge 1 && $MTP_PORT -le 65535 ]]; then
@@ -15,29 +10,30 @@ while true; do
     fi
 done
 
-# 生成或读取用户输入的 MTP_SECRET
-read -p "是否生成随机的 MTP_SECRET？(Y/n): " generate_secret
-if [[ $generate_secret =~ ^[Yy]$ || -z $generate_secret ]]; then
-    MTP_SECRET=$(generate_random_secret)
-    echo "生成的 MTP_SECRET 为: $MTP_SECRET"
-else
-    while true; do
-        read -p "请输入 MTP_SECRET (32 个十六进制字符): " MTP_SECRET
-        if [[ $MTP_SECRET =~ ^[0-9a-fA-F]{32}$ ]]; then
-            break
-        else
-            echo "无效的 MTP_SECRET，请输入 32 个十六进制字符。"
-        fi
-    done
-fi
+# 生成随机的 MTP_SECRET
+while true; do
+    read -p "按下回车键以生成随机的 MTP_SECRET，或者输入自定义的 MTP_SECRET: " input_secret
+    if [[ -z $input_secret ]]; then
+        MTP_SECRET=$(openssl rand -hex 16)
+        echo "生成的随机 MTP_SECRET 为: $MTP_SECRET"
+        break
+    elif [[ $input_secret =~ ^[0-9a-fA-F]{32}$ ]]; then
+        MTP_SECRET=$input_secret
+        break
+    else
+        echo "无效的 MTP_SECRET，请输入 32 个十六进制字符。"
+    fi
+done
 
 # 设置默认的 MTP_TAG
 MTP_TAG="f661069514b5fde9c00201a12a030c3e"
 
-# 提示用户输入 MTP_TAG
-read -p "请输入 MTP_TAG (32 个十六进制字符，默认为 $MTP_TAG): " tag_input
-if [[ ! -z $tag_input && $tag_input =~ ^[0-9a-fA-F]{32}$ ]]; then
-    MTP_TAG=$tag_input
+# 提示用户输入 MTP_TAG，如果留空则使用默认值
+read -p "请输入 MTP_TAG (32 个十六进制字符，留空以使用默认值): " input_tag
+if [[ ! -z $input_tag && $input_tag =~ ^[0-9a-fA-F]{32}$ ]]; then
+    MTP_TAG=$input_tag
+elif [[ ! -z $input_tag ]]; then
+    echo "无效的 MTP_TAG，将使用默认值: $MTP_TAG"
 fi
 
 # 检查并安装 Docker
